6784f54fa23524acc7d989c2c7ed0e43854af953
Florent Collot <florent.collot@ihu-liryc.fr>
2021-02-05 15:45:08 +0100

    [medPython] error handling

---
+++ src/layers/medPython/core/medPythonCoreForward.h
@@ -0,0 +1,31 @@
+#pragma once
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2021. All rights reserved.
+ See LICENSE.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
+
+// This header forwards some elements of the CPython library to avoid having
+// to link with it when including other medPython headers.
+
+#include "medPythonExport.h"
+
+typedef struct _object PyObject;
+
+namespace med::python
+{
+
+// Increases the reference count of a python object if it is non-null
+MEDPYTHON_EXPORT void incref(PyObject* object);
+
+// Decreases the reference count of a python object if it is non-null
+MEDPYTHON_EXPORT void decref(PyObject* object);
+
+} // namespace med::python
