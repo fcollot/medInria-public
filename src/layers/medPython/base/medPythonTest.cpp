fc4d3d4113a3d7cd94f73b4579fe3dec373e7370
Florent Collot <florent.collot@ihu-liryc.fr>
2022-04-13 14:16:49 +0200

    [medPython] adapt to new resource system, init with Python home

--- src/layers/medPython/base/medPythonTest.cpp
+++ src/layers/medPython/base/medPythonTest.cpp
@@ -46,14 +46,11 @@ void printEmptyLine()
 
 bool isEmbeddedPath(QString path)
 {
-    static QStringList embeddedPathRoots = getTemporaryDirectories() << med::getExternalResourcesDirectory(TARGET_NAME);
+    static QString embeddedPathRoot = med::getExternalResourcesDirectory(".", TARGET_NAME);
 
-    foreach (QString pathRoot, embeddedPathRoots)
+    if (path.startsWith(embeddedPathRoot))
     {
-        if (path.startsWith(pathRoot))
-        {
-            return true;
-        }
+        return true;
     }
 
     return false;
@@ -219,7 +216,7 @@ void runEmbeddedPythonTest()
     printEmptyLine();
     testModuleImport("json");
     printEmptyLine();
-    //testSysPath();
+    testSysPath();
     printEmptyLine();
     testSysModules();
     printEmptyLine();
